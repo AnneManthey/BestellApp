bff7b31ffc6b94e55b51c46496eb033c527ad972
Anne Manthey <anne.manthey@pm.me>
2026-05-27 15:07:54 +0200

    add menu card html&css

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+// To Do:
+
+
+// Menü Card erstellen (HTML & CSS)
+// JSON Arrays erstellen
+// Init Function & Templates rendern
+// Basket logik 
+// Basket rendern
+// Message Danke für Bestellung
+
+// Responsive Basket / Footer
+// Responsive Media
+// Imprint & Cookies
