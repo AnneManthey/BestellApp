d8cb4a776b2d24cf7d256193f8be97434b506d9f
Anne Manthey <anne.manthey@pm.me>
2026-06-02 08:31:59 +0200

    minor css

--- script.js
+++ script.js
@@ -214,12 +214,12 @@ function closeDialogBasket() {
 
 
 
-// responsive footer orderzahl fixen
+
 // Optional: Preis mit/ohne Lieferkosten berechnen (if/else bei über 50€ warenwert)
 
-// CSS Dialog Basket fixen
 // Pfeile Kategorieleisten drehen
 
+// JS funktionen kürzen/aufteilen?
 
 // Imprint & Cookies
 
